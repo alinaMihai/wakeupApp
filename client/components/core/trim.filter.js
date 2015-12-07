abc0107dc7fd18220a1c54c7d24fcf7f88629e25
Alina <alina.mihai90@gmail.com>
2015-12-07 16:30:24 +0200

    create and use trimfilter to shorten text for topic and quote preview

---
+++ client/components/core/trim.filter.js
@@ -0,0 +1,13 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('wakeupApp')
+        .filter('trimFilter', trimFilter);
+
+    function trimFilter() {
+        return function(text) {
+            return text.length > 250 ? text.slice(0, 250) + "..." : text;
+        };
+    }
+})();
