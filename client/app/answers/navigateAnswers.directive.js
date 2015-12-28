97bcbef282b1eff3afc1debd4e6129a7b2d8c70b
Alina <alina.mihai90@gmail.com>
2015-12-28 11:46:27 +0200

    add arrow navigation through answers

---
+++ client/app/answers/navigateAnswers.directive.js
@@ -0,0 +1,54 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('wakeupApp')
+        .directive('navigateArrows', directive);
+
+    directive.$inject = ['$state'];
+
+    /* @ngInject */
+    function directive($state) {
+        var directive = {
+            link: link,
+            restrict: 'E',
+            scope: {
+                prevId: '=',
+                nextId: '='
+            }
+        };
+        return directive;
+
+        function link(scope, element, attrs) {
+            $('html').keydown(function(e) {
+                switch (e.which) {
+                    case 39:
+                        {
+                            if (scope.nextId) {
+                                $state.go('answerList', {
+                                    'questionId': scope.nextId
+                                });
+                            }
+
+                            break;
+                        }
+                    case 37:
+                        {
+                            if (scope.prevId) {
+                                $state.go('answerList', {
+                                    'questionId': scope.prevId
+                                });
+                            }
+
+                            break;
+                        }
+                }
+            });
+        }
+    }
+
+    /* @ngInject */
+    function Controller() {
+
+    }
+})();
